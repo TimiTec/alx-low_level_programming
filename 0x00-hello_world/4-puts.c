cdc5217a238f11ff9d5c417248ff01d0170b9849
Tim <williamstimothy016@gmail.com>
2022-12-08 19:27:19 -0800

    More corrections

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
+/*
+ * The program outputs a string of characters
+ */
 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
