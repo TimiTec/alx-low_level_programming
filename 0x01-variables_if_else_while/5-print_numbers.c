f65035956b90f52d00ae1c0b2ba982691c79c0df
Tim <williamstimothy016@gmail.com>
2022-12-11 00:35:29 -0800

    Adding Print numbers task

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints single digit base ten numbers
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		printf("%d", num);
+	}
+
+	return (0);
+}
