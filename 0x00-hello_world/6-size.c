f2997202b67554c4858ba0e879898baaee8f1409
Tim <williamstimothy016@gmail.com>
2022-12-12 08:37:20 -0800

    Adding files

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -19,7 +19,7 @@ int main(void)
 
 	printf("Size of a char: %lu byte(s)\n", sizeof(ch));
 
-	printf("Size of an int: %lu bytes(s)\n", sizeof(inttype));
+	printf("Size of an int: %lu byte(s)\n", sizeof(inttype));
 
 	printf("Size of a long int: %lu byte(s)\n", sizeof(longtype));
 
