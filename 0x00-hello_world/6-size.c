69b4c6e249230cb111eef2cd801add442ec4ddb2
Tim <williamstimothy016@gmail.com>
2022-12-12 07:37:48 -0800

    Adding corrections to 6 size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the size of various types
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	int inttype;
+
+	long longtype;
+
+	long double longtypedouble;
+
+	float floattype;
+
+	printf("Size of a char: %lu byte(s)\n", sizeof(ch));
+
+	printf("Size of an int: %lu bytes(s)\n", sizeof(inttype));
+
+	printf("Size of a long int: %lu byte(s)\n", sizeof(longtype));
+
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(longtypedouble));
+
+	printf("Size of a float: %lu byte(s)\n", sizeof(floattype));
+
+	return (0);
+}
