2ea4c4635f7b1ced6d78ddff1b91bed804bb78d8
Tim <williamstimothy016@gmail.com>
2022-12-14 16:23:33 -0800

    Alphabet corrections

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,19 @@
-#include 'main.h'
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+
+}
