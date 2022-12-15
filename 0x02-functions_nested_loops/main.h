90999a2615139994bfb3096b1f93c43d5b406134
Tim <williamstimothy016@gmail.com>
2022-12-14 18:53:56 -0800

    Corrections to main header file

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,5 @@
 #include <unistd.h>
+#include "_putchar.c"
 
 int _putchar(char c);
 void print_alphabet(void);
@@ -11,7 +12,5 @@ int print_last_digit(int n);
 void jack_bauer(void);
 void times_table(void);
 int add(int a, int b);
-void print_tp_98(int n);
+void print_to_98(int n);
 void print_times_table(int n);
-
-
